89a17426ea48666c947058bc264a90b08822aa82
SprutSDM <zakoylov@gmail.com>
2018-02-15 22:14:12 +0300

    fix lab6.py

--- lab6.py
+++ lab6.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from bs4 import BeautifulSoup
 import requests
+from bottle import route, run, template, request, post, get, redirect
 
 
 Base = declarative_base()
@@ -72,10 +73,25 @@ def update_news(s):
         i += 1
     s.commit()
 
+
+@route('/news')
+def news_list():
+    s = session()
+    rows = s.query(News).filter(News.label == None).all()
+    return template('news_template', rows=rows)
+
+@route('/add_label/')
+def add_label():
+    label = request.query.label
+    ID = request.query.id
+    print(label, ID)
+    redirect('/news')
+
 engine = create_engine("sqlite:///news.db")
 Base.metadata.create_all(bind=engine)
 session = sessionmaker(bind=engine)
 s = session()
 url = 'https://habrahabr.ru/flows/develop/all/'
+
+run(host='localhost', port=8080)
 #get_all_news(s)
-#s.commit()
